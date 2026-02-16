4851ca8b720c1ddc832495224971de9936b3c9b4
sample-maxx <252699876+sample-maxx@users.noreply.github.com>
2026-02-16 19:32:51 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import http from 'http';
+import fs from 'fs';
+import path from 'path';
+
+const server = http.createServer((req, res) => {
+  let filePath = './index.html';
+
+  if (req.url === '/about'|| req.url==='/about.html') {
+    filePath = './about.html';
+  }else if (req.url===''||req.url==='/' ||req.url === '/index' || req.url==='/index.html') {
+    filePath = './index.html';
+  } else if (req.url === '/contact'|| req.url==='/contact-me'||req.url==='/contact.html' || req.url==='/contact-me.html') {
+    filePath = './contact-me.html';
+  }else{
+    filePath = './404.html';
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('File not found');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
+});
+server.listen(8080, () => {
+  console.log('Server running at http://localhost:8080');
+});
