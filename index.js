cd19004e3bcb902089fdf37b7b85acf8203adf40
sample-maxx <252699876+sample-maxx@users.noreply.github.com>
2026-02-16 21:50:21 +0530

    Code changes

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import fs from 'fs';
 import path from 'path';
 
 const server = http.createServer((req, res) => {
-  let filePath = './index.html';
+  let filePath = './infoSite/index.html';
 
   if (req.url === '/about'|| req.url==='/about.html') {
-    filePath = './about.html';
+    filePath = './infoSite/about.html';
   }else if (req.url===''||req.url==='/' ||req.url === '/index' || req.url==='/index.html') {
-    filePath = './index.html';
+    filePath = './infoSite/index.html';
   } else if (req.url === '/contact'|| req.url==='/contact-me'||req.url==='/contact.html' || req.url==='/contact-me.html') {
-    filePath = './contact-me.html';
+    filePath = './infoSite/contact-me.html';
   }else{
     filePath = './404.html';
   }
